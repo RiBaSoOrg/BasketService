eb52476f332bfbe8f9fa01c2f7271485b85bae26
Rico <rico.flemming@student.htw-berlin.de>
2024-07-04 14:59:51 +0200

    jsoncreator versuch

--- basketservice/src/main/java/com/ribaso/basketservice/core/domain/model/Book.java
+++ basketservice/src/main/java/com/ribaso/basketservice/core/domain/model/Book.java
@@ -12,25 +12,41 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Entity
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Book {
     @Id
     private String id;
     private String title;
     private String subtitle;
     private String isbn;
-    private String abstractText;  
+    private String abstractText;
     private String author;
     private String publisher;
     private String price;
     private int numPages;
 
-    
+    @JsonCreator // This annotation helps in guiding Jackson how to construct an instance of Book
+    public Book(@JsonProperty("id") String id,
+            @JsonProperty("title") String title,
+            @JsonProperty("subtitle") String subtitle,
+            @JsonProperty("isbn") String isbn,
+            @JsonProperty("abstractText") String abstractText,
+            @JsonProperty("author") String author,
+            @JsonProperty("publisher") String publisher,
+            @JsonProperty("price") String price,
+            @JsonProperty("numPages") int numPages) {
+        this.id = id;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.isbn = isbn;
+        this.abstractText = abstractText;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+        this.numPages = numPages;
+    }
+
 }
-
-
