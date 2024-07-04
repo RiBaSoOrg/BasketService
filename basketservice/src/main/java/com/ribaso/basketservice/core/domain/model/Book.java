79a7fb73d0b13352c9fefc2e8b10317468dd16e8
Rico <rico.flemming@student.htw-berlin.de>
2024-07-04 12:23:52 +0200

    Jsoncreator

--- basketservice/src/main/java/com/ribaso/basketservice/core/domain/model/Book.java
+++ basketservice/src/main/java/com/ribaso/basketservice/core/domain/model/Book.java
@@ -2,6 +2,9 @@ package com.ribaso.basketservice.core.domain.model;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -9,4 +12,16 @@ public class Book {
     private String id;
     private String title;
     private BigDecimal price;
+
+
+ @JsonCreator
+    public Book(@JsonProperty("id") String id,
+    @JsonProperty("title") String title,
+    @JsonProperty("price") BigDecimal price){
+        this.id = id; 
+        this.title = title;
+        this.price = price;
+    }
 }
+
+
