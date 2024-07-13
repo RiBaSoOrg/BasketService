461435fdb691abca730ac836ffbe12041ab4f5de
Rico <rico.flemming@student.htw-berlin.de>
2024-07-13 12:43:43 +0200

    rabbitMQconfig richtiger ordner

--- basketservice/src/main/java/com/ribaso/basketservice/port/config/RabbitMQConfig.java
+++ basketservice/src/main/java/com/ribaso/basketservice/port/config/RabbitMQConfig.java
@@ -1,4 +1,4 @@
-package com.ribaso.basketservice.port.basket.producer;
+package com.ribaso.basketservice.port.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
