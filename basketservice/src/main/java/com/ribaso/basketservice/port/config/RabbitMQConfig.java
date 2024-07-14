dea127d5ab9b81f4f8c4459376e7f0cea93e6ccb
Rico <rico.flemming@student.htw-berlin.de>
2024-07-14 14:12:25 +0200

    rabbitMQ sendandrecive 0.4

--- basketservice/src/main/java/com/ribaso/basketservice/port/config/RabbitMQConfig.java
+++ basketservice/src/main/java/com/ribaso/basketservice/port/config/RabbitMQConfig.java
@@ -1,5 +1,8 @@
 package com.ribaso.basketservice.port.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
@@ -8,6 +11,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,7 +36,21 @@ public class RabbitMQConfig {
 
     @Bean
     public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setClassMapper(typeMapper());
+        return converter;
+    }
+
+    @Bean
+    public DefaultClassMapper typeMapper() {
+        DefaultClassMapper typeMapper = new DefaultClassMapper();
+        Map<String, Class<?>> idClassMapping = new HashMap<>();
+        // Zuordnung des Typs aus dem 'bookService' zum lokalen 'Book' in
+        // 'basketService'
+        idClassMapping.put("com.ribaso.bookservice.core.domain.model.Book",
+                com.ribaso.basketservice.core.domain.model.Book.class);
+        typeMapper.setIdClassMapping(idClassMapping);
+        return typeMapper;
     }
 
     @Bean
